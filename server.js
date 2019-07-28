b672f2f8f99aecc8d3d8ba8552c806880ae051da
lara bergerson <lara.bergerson@gmail.com>
2019-07-28 12:09:09 -0500

    set up files, port working and path complete

---
+++ server.js
@@ -0,0 +1,82 @@
+// Dependencies:
+
+var express = require("express");
+var mongojs = require("mongojs");
+
+// require Cheerio and Axios to make the web scraping possible:
+
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// initialize Express:
+var app = express();
+
+// DB config:
+var databaseUrl = "scraped";
+var collections = ["scrapedData"];
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+  console.log("DB ERROR:", error);
+});
+
+app.get("/", function (req, res) {
+  db.scrapedData.find({}, function (error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.theonion.com/").then(function (response) {
+    var $ = cheerio.load(response.data);
+    console.log("grabbing threads...");
+
+    // var results = [];
+
+    $(".title").each(function (i, element) {
+        
+      var title = $(element).children("a").text();
+      var summary = $(element).text();
+      var link = $(element).children().attr("href");
+      
+      if (title && link) {
+        db.scrapedData.insert({
+          title: title,
+          summary: summary,
+          link: link
+        },
+          function (err, inserted) {
+            if (err) {
+              console.log(err)
+            }
+            else {
+              console.log(inserted);
+            }
+          });
+      }
+    });
+  });
+  res.send("Scraped web successfully");
+
+});
+app.listen(3000, function () {
+  console.log("App listening on port: 3000");
+});
+
+      
+      // results.push({
+        //   title: title,
+        //   summary: summary,
+        //     link: link
+        //   });
+        // });
+      
+    //     console.log(results);
+    // });
+    
+
---
+++ server2.js
@@ -0,0 +1,40 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// First, tell the console what server2.js is doing
+console.log("\n---------------------------------------\n" +
+            "Scraping every article headline and link\n" +
+            "from the Onion website" +
+            "\n---------------------------------------\n");
+
+// Making a request via axios for `nhl.com`'s homepage
+axios.get("https://www.theonion.com/").then(function(response) {
+
+  // Load the body of the HTML into cheerio
+  var $ = cheerio.load(response.data);
+
+  // Empty array to save our scraped data
+  var results = [];
+
+  // With cheerio, find each h4-tag with the class "headline-link" and loop through the results
+  $("headline-link").each(function(i, element) {
+
+    // Save the text of the h4-tag as "title"
+    var title = $(element).text();
+
+    var summary = $(element).text();
+
+    // Find the h4 tag's parent a-tag, and save it's href value as "link"
+    var link = $(element).parent().attr("href");
+
+    // Make an object with data we scraped for this h4 and push it to the results array
+    results.push({
+      title: title,
+      summary: summary,
+      link: link
+    });
+  });
+
+  // After looping through each h4.headline-link, log the results
+  console.log(results);
+});
